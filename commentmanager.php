3775ae0a81e50b8e0c08066e136570434ed28fa0
CarolineHugues <36641185+CarolineHugues@users.noreply.github.com>
2018-02-26 21:25:36 +0100

    Création classe CommentManager

---
+++ commentmanager.php
@@ -0,0 +1,59 @@
+<?php
+
+class CommentManager {
+
+	private  $_db;
+
+	public function contruct($db) {
+
+	}
+
+	public function add(comment $comment) {
+		/*Préparer la requête*/
+		/*Assigner les valeurs*/
+		/*Exécuter la requête*/
+
+	}
+
+	public function delete($id) {
+		/*Récupérer l'identifiant de type int*/
+		/*Préparer la requête*/
+		/* ... */
+		/*Exécuter la requête*/
+
+	}
+
+	public function countComments() {
+		/*Requête : compter le nombre de commentaires */
+
+	}
+
+	public function countReportedComments() {
+		/*Requête : compter le nombre de commentaires signalés*/
+
+	}
+
+
+	/**
+	 * Les utilisateurs peuvent signaler les commentaires indésirables
+	 */
+	public function reportComment() {
+		/*Modifier le statut en signalé*/
+
+	}
+
+	public function get($id) {
+
+	}
+
+	public function getList() {
+
+	}
+
+	public function setDb(PDO $db) {
+
+	}
+
+
+}
+?>
