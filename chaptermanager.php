a42019780ad536db7775243e104111238e8390ba
Caroline Hugues <hugues.caroline9@gmail.com>
2018-03-01 09:11:38 +0100

    Classe chapterManager complétée

--- chaptermanager.php
+++ chaptermanager.php
@@ -1,87 +1,192 @@
 <?php
 
-class ChapterManager {
+class ChapterManager 
+{
+	private  $_db;
 
-
-	private  $_bd;
-
-	public function __construct($db) {
-
+	public function __construct($db) 
+	{
+		$this->setDb($db);
 	}
 
-	public function add(chapter $chapter) {
-		/*Préparer la requête*/
-		/*Assigner les valeurs*/
-		/*Exécuter la requête*/
-
+	public function setDb(PDO $db) 
+	{
+		$this->_db = $db;
 	}
 
-	public function update(chapter $chapter) {
-		/*Préparer la requête*/
-		/*Modifier les valeurs*/
-		/*Exécuter la requête*/
+	public function add(Chapter $chapter) 
+	{
+		$requete = $this->db->prepare('INSERT INTO chapters (title, author, content, excerpt, addDate, updateDate, publishedDate, status) VALUES(:title, :author, :content, :excerpt, NOW(), NOW(), NOW(), :status)');
 
+		$requete->bindValue(':title', $chapter->title());
+		$requete->bindValue(':author', $chapter->author());
+		$requete->bindValue(':content', $chapter->content());
+		$requete->bindValue(':excerpt', $chapter->excerpt());
+		$requete->bindValue(':status', $chapter->status());
+
+		$requete->execute();
 	}
 
-	public function delete($id) {
-		/*Récupérer l'identifiant de type int*/
-		/*Préparer la requête*/
-		/* ... */
-		/*Exécuter la requête*/
+	public function update(Chapter $chapter) 
+	{
+		$requete = $this->db->prepare('UPDATE chapters SET title = :title, author = :author, content = :content, excerpt = :excerpt, updateDate = NOW(), publishedDate = NOW(), status = :status WHERE id = :id');
 
+		$requete->bindValue(':title', $chapter->title());
+		$requete->bindValue(':author', $chapter->author());
+		$requete->bindValue(':content', $chapter->content());
+		$requete->bindValue(':excerpt', $chapter->excerpt());
+		$requete->bindValue(':status', $chapter->status());
+		$requete->bindValue(':id', $chapter->id(), PDO::PARAM_INT);
+
+		$requete->execute();
 	}
 
-	public function save(chapter $chapter) {
-		/*Vérifier la validité du chapitre*/
-		/*S'il est nouveau : l'ajouter, s'il existe déjà : le modifier*/
-
+	public function delete($id) 
+	{+
+		$this->db->exec('DELETE FROM chapters WHERE id = '.(int) $id);
 	}
 
-	public function publish(chapter $chapter) {
-
-
+	public function save(Chapter $chapter) 
+	{
+		if ($chapter->isValid())
+		{
+			$chapter->isNew() ? $this->add($chapter) : $this->update($chapter);
+		}
+		else
+		{
+			throw new RuntimeException('Le chapitre doit être valide pour être enregistrée');
+		}
 	}
 
-	public function countDrafts() {
-		/*Requête : compter le nombre de chapitres dont le statut est 'draft'*/
+	public function publish(Chapter $chapter) 
+	{
+		$status = 'published';
 
+		$requete = $this->db->prepare('UPDATE chapters SET status = :status WHERE id = :id');
+
+		$requete->bindValue(':status', $status, PDO::PARAM_STR);
+		$requete->bindValue(':id', $chapter->id(), PDO::PARAM_INT);
+
+		$requete->execute();
 	}
 
-	public function countPublished() {
-		/*Requête : compter le nombre de chapitres dont le statut est 'published'*/
-
+	public function countDrafts() 
+	{
+		{
+			return $this->db->query('SELECT COUNT(*) FROM chapters WHERE status = \'draft\'')->fetchColumn(); 
+		}
 	}
 
-	public function countChapters() {
-		/*Requête : compter le nombre de chapitres total*/
-
+	public function countPublished() 
+	{
+		{
+			return $this->db->query('SELECT COUNT(*) FROM chapters WHERE status = \'published\'')->fetchColumn(); 
+		}
 	}
 
-
-	/**
-	 * Obtenir un chapitre selon son identifiant
-
-	 */
-	public function getUnique($id) {
-		/*Récupérer l'identifiant de type int*/
-		/*Préparer la requête*/
-		/* ... */
-		/*Exécuter la requête*/
-
-
+	public function countChapters() 
+	{
+		public function count()
+		{ 
+			return $this->db->query('SELECT COUNT(*) FROM chapters')->fetchColumn();  
+		}
 	}
 
-	/**
-	 * Obtenir la liste de tous les chapitres
-	 */
-	public function getList() {
+	public function getUnique($id) 
+	{
+		$requete = $this->db->prepare('SELECT id, title, author, content, excerpt, addDate, updateDate, publishedDate, status FROM chapters WHERE id = :id');
 
+		$requete->bindValue(':id', (int) $id, PDO::PARAM_INT);
+
+		$requete->execute();
+
+		$requete->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, '\Entity\Chapter');
+
+		$chapter = $requete->fetch();
+
+		$chapter->setAddDate(new DateTime($chapter->AddDate()));
+		$chapter->setUpdateDate(new DateTime($chapter->updateDate()));
+		$chapter->setPunlishedDate(new DateTime($chapter->publishedDate()));
+
+		return $chapter;
 	}
 
-	public function setDb(PDO $db) {
+	public function getDraftsList($start = -1, $limit = -1) 
+	{
+		$sql = 'SELECT id, title, author, content, excerpt, addDate, updateDate, publishedDate, status FROM chapters WHERE status = \'draft\' ORDER BY id DESC';
 
+		if ($start != -1 || $limit != -1)
+			{
+				$sql .= ' LIMIT '.(int) $limit.' OFFSET '.(int) $start;
+			}
+
+		$requete = $this->db->query($sql);
+		$requete->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, '\Entity\Chapter');
+
+		$DraftsList = $requete->fetchAll();
+
+		foreach ($DraftsList as $chapter)
+		{
+			$chapter->setAddDate(new DateTime($chapter->AddDate()));
+			$chapter->setUpdateDate(new DateTime($chapter->updateDate()));
+			$chapter->setPublishedDate(new DateTime($chapter->publishedDate()));
+		}
+
+		$requete->closeCursor();
+
+		return $DraftsList;
 	}
 
+	public function getPublishedList($start = -1, $limit = -1) 
+	{
+		$sql = 'SELECT id, title, author, content, excerpt, addDate, updateDate, publishedDate, status FROM chapters WHERE status = \'published\' ORDER BY id DESC';
+
+		if ($start != -1 || $limit != -1)
+			{
+				$sql .= ' LIMIT '.(int) $limit.' OFFSET '.(int) $start;
+			}
+
+		$requete = $this->db->query($sql);
+		$requete->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, '\Entity\Chapter');
+
+		$PublishedChaptersList = $requete->fetchAll();
+
+		foreach ($PublishedChaptersList as $chapter)
+		{
+			$chapter->setAddDate(new DateTime($chapter->AddDate()));
+			$chapter->setUpdateDate(new DateTime($chapter->updateDate()));
+			$chapter->setPublishedDate(new DateTime($chapter->publishedDate()));
+		}
+
+		$requete->closeCursor();
+
+		return $PublishedChaptersList;
+	}
+
+	public function getList($start = -1, $limit = -1) 
+	{
+		$sql = 'SELECT id, title, author, content, excerpt, addDate, updateDate, publishedDate, status FROM chapters ORDER BY id DESC';
+
+		if ($start != -1 || $limit != -1)
+			{
+				$sql .= ' LIMIT '.(int) $limit.' OFFSET '.(int) $start;
+			}
+
+		$requete = $this->db->query($sql);
+		$requete->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, '\Entity\Chapter');
+
+		$chaptersList = $requete->fetchAll();
+
+		foreach ($chaptersList as $chapter)
+		{
+			$chapter->setAddDate(new DateTime($chapter->AddDate()));
+			$chapter->setUpdateDate(new DateTime($chapter->updateDate()));
+			$chapter->setPublishedDate(new DateTime($chapter->publishedDate()));
+		}
+
+		$requete->closeCursor();
+
+		return $chaptersList;
+	}
 
 }
-?>
