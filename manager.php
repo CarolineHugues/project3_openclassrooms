7d278e492a8f1bcf07bb69872d8e0991648a5b11
Caroline Hugues <hugues.caroline9@gmail.com>
2018-03-01 09:36:40 +0100

    Création de la classe Manager

---
+++ manager.php
@@ -0,0 +1,16 @@
+<?php
+
+abstract class Manager 
+{
+	private  $_db;
+
+	public function __construct($db) 
+	{
+		$this->setDb($db);
+	}
+
+	public function setDb(PDO $db) 
+	{
+		$this->_db = $db;
+	}
+}
