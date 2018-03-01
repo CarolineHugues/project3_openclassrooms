afa74d0f542698a673e8fd4edf59a76dc45e9da7
Caroline Hugues <hugues.caroline9@gmail.com>
2018-03-01 09:15:36 +0100

    Classe commentManager complétée

--- commentmanager.php
+++ commentmanager.php
@@ -1,59 +1,121 @@
 <?php
 
-class CommentManager {
-
+class CommentManager 
+{
 	private  $_db;
 
-	public function __construct($db) {
-
+	public function __construct($db) 
+	{
+		$this->setDb($db);
 	}
 
-	public function add(comment $comment) {
-		/*Préparer la requête*/
-		/*Assigner les valeurs*/
-		/*Exécuter la requête*/
-
+	public function setDb(PDO $db) 
+	{
+		$this->_db = $db;
 	}
 
-	public function delete($id) {
-		/*Récupérer l'identifiant de type int*/
-		/*Préparer la requête*/
-		/* ... */
-		/*Exécuter la requête*/
+	public function add(Comment $comment) 
+	{
+		$requete = $this->db->prepare('INSERT INTO comments (author, content, publishedDate, authorMail, status) VALUES(:author, :content, NOW(), :authorMail, :status)');
 
+		$requete->bindValue(':author', $comments->author());
+		$requete->bindValue(':content', $comments->content());
+		$requete->bindValue(':authorMail', $comments->authorMail());
+		$requete->bindValue(':status', $comments->status());
+
+		$requete->execute();
 	}
 
-	public function countComments() {
-		/*Requête : compter le nombre de commentaires */
-
+	public function delete($id) 
+	{
+		$this->db->exec('DELETE FROM comments WHERE id = '.(int) $id);
 	}
 
-	public function countReportedComments() {
-		/*Requête : compter le nombre de commentaires signalés*/
-
+	public function countComments() 
+	{
+		return $this->db->query('SELECT COUNT(*) FROM comments')->fetchColumn(); 
 	}
 
-
-	/**
-	 * Les utilisateurs peuvent signaler les commentaires indésirables
-	 */
-	public function reportComment() {
-		/*Modifier le statut en signalé*/
-
+	public function countReportedComments() 
+	{
+		return $this->db->query('SELECT COUNT(*) FROM comments WHERE status = \'reported\'')->fetchColumn();
 	}
 
-	public function get($id) {
+	public function reportComment() 
+	{
+		$status = 'reported';
 
+		$requete = $this->db->prepare('UPDATE comments SET status = :status WHERE id = :id');
+
+		$requete->bindValue(':status', $status, PDO::PARAM_STR);
+		$requete->bindValue(':id', $comment->id(), PDO::PARAM_INT);
+
+		$requete->execute();
 	}
 
-	public function getList() {
+	public function getUnique($id) 
+	{
+		$requete = $this->db->prepare('SELECT id, author, content, publishedDate, authorMail, status FROM comments WHERE id = :id');
 
+		$requete->bindValue(':id', (int) $id, PDO::PARAM_INT);
+
+		$requete->execute();
+
+		$requete->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, '\Entity\Comment');
+
+		$comment = $requete->fetch();
+
+		$comment->setPublishedDate(new DateTime($comment->publishedDate()));
+
+		return $comment;
 	}
 
-	public function setDb(PDO $db) {
+	public function getList($start = -1, $limit = -1) 
+	{
+		$sql = 'SELECT id, author, content, publishedDate, authorMail, status FROM comments ORDER BY id DESC';
 
+		if ($start != -1 || $limit != -1)
+			{
+				$sql .= ' LIMIT '.(int) $limit.' OFFSET '.(int) $start;
+			}
+
+		$requete = $this->db->query($sql);
+		$requete->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, '\Entity\Comment');
+
+		$commentsList = $requete->fetchAll();
+
+		foreach ($commentsList as $comment)
+		{
+			$comment->setPublishedDate(new DateTime($comment->publishedDate()));
+		}
+
+		$requete->closeCursor();
+
+		return $commentsList;
 	}
 
+	public function getReportedList($start = -1, $limit = -1) 
+	{
+		$sql = 'SELECT id, author, content, publishedDate, authorMail, status FROM comments WHERE status = \'reported\' ORDER BY id DESC';
+
+		if ($start != -1 || $limit != -1)
+			{
+				$sql .= ' LIMIT '.(int) $limit.' OFFSET '.(int) $start;
+			}
+
+		$requete = $this->db->query($sql);
+		$requete->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, '\Entity\Comment');
+
+		$reportedList = $requete->fetchAll();
+
+		foreach ($reportedList as $comment)
+		{
+			$comment->setPublishedDate(new DateTime($comment->publishedDate()));
+		}
+
+		$requete->closeCursor();
+
+		return $reportedList;
+	}
 
 }
-?>
