3a84e80356fe257d897b2ad1662b1f964483b504
Caroline Hugues <hugues.caroline9@gmail.com>
2018-03-13 15:50:18 +0100

    Création du dossier Controllers

---
+++ Controllers/frontendcontroller.php
@@ -0,0 +1,42 @@
+<?php
+
+require_once 'Models/chaptermanager.php';
+require_once 'Models/commentmanager.php';
+require_once 'Views/view.php';
+
+class FrontendController {
+
+	private $_chapterManager,
+			$_commentManager;
+
+  	public function __construct() 
+  	{
+    	$this->_chapterManager = new ChapterManager ();
+    	$this->_commentManager = new CommentManager ();
+  	}
+
+	public function listChapters()
+	{
+    	$chapters = $this->_chapterManager->getPublishedList(0,3);
+    	$view = new View('listChapters');
+    	$view->generate(array('chapters' => $chapters));
+	}
+
+	public function chapter($id)
+	{
+		$chapter = $this->_chapterManager->getUnique($id);
+    	$comments = $this->_commentManager->getListOf($chapter->id());
+    	$view = new View('chapter');
+    	$view->generate(array('chapter' => $chapter, 'comments' => $comments));
+	}
+
+	public function addcomment(Comment $comment)
+	{
+		$this->_commentManager->add(Comment $comment);
+		$this->chapter($chapter->id()); //Actualisation de l'affichage du billet
+	}
+
+}
+
+
+
---
+++ Controllers/router.php
@@ -0,0 +1,49 @@
+<?php
+require_once 'Controllers/frontendcontroller.php';
+
+class Router 
+{
+    private $_frontendController;
+
+    public function __construct() 
+    {
+        $this->_frontendController = new FrontendController ();
+    }
+
+    public function routerRequest() 
+    {
+        try
+        {
+            if (isset($_GET['action'])) 
+            {
+                if ($_GET['action'] == 'chapter') 
+                {
+                    if (isset($_GET['id']) && $_GET['id'] > 0) 
+                    {
+                        $id = intval($_GET['id']);
+                        $this->_frontendController->chapter($id);
+                    }
+                    else
+                    {
+                       throw new Exception("Identifiant de chapitre non valide ou non défini"); 
+                    }
+                }    
+                else if ($_POST['action'] == 'addComment') 
+                {
+                    $this->_frontendController->addComment();
+                }    
+                else 
+                {
+                    throw new Exception("Action non valide");    
+                }
+            }           
+			else 
+            {
+                $this->_frontendController->listChapters();
+            }
+        catch (Exception $e) 
+        {
+            $this->error($e->getMessage());
+        }
+    }
+}
