764ee519c19a51b36d46b61d57bef3af34bf55a2
Caroline Hugues <hugues.caroline9@gmail.com>
2018-03-16 20:51:16 +0100

    Corrections et ajout du signalement des commentaires indésirables

--- Views/chapterView.php
+++ Views/chapterView.php
@@ -25,16 +25,23 @@ ob_start(); ?>
 		<?php foreach ($comments as $comment)
 		{ 
 		?>
-			<p><?= $comment->author ?></p>
+			<p><?= htmlspecialchars($comment->author) ?></p>
 			<p><?= $comment->publishedDate ?></p>
-			<p><?= $comment->content ?></p>	
+			<p><?= htmlspecialchars(nl2br($comment->content)) ?></p>
+			<p><?= $comment->status ?></p>
+			<form method="post" action="?action=reportComments">
+				<input type="hidden" name="id" value="<?= $comment->id() ?>" />
+				<input type="hidden" name="chapterid" value="<?= $comment->chapterId ?>" />
+				<input value="Signaler ce commentaire" type="submit">
+			</form>
 		<?php			
 		}
 		?>
 	</section>
 
 	<section>
-		<form method="post" action="#">
+		<h2>Laisser un commentaire</h2>
+		<form method="post" action="?action=addComment">
 			<div class="field">
 				<label for="author">Nom</label>
 				<input id="name" name="author" type="text">
@@ -47,6 +54,7 @@ ob_start(); ?>
 				<label for="content">Commentaire</label>
 				<textarea id="comment" name="content" rows="3"></textarea>
 			</div>
+			<input type="hidden" name="id" value="<?= $chapter->id() ?>" />
 			<ul class ="actions">
 				<li>
 					<input value="Laisser un commentaire" type="submit">
