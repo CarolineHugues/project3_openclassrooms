fc810ea94f62e8bdf4037984d1f360c28cd0ed7e
Caroline Hugues <hugues.caroline9@gmail.com>
2018-03-19 21:39:16 +0100

    Modification

--- Views/chapterView.php
+++ Views/chapterView.php
@@ -25,10 +25,12 @@
 		<?php foreach ($comments as $comment)
 		{ 
 		?>
+			<div class="box">
 			<p><?= htmlspecialchars($comment->author) ?></p>
 			<p><?= $comment->publishedDate_fr ?></p>
 			<p><?= htmlspecialchars(nl2br($comment->content)) ?></p>
 			<p><?= $comment->status ?></p>
+			</div>
 			<form method="post" action="?action=reportComments">
 				<input type="hidden" name="id" value="<?= $comment->id() ?>" />
 				<input type="hidden" name="chapterid" value="<?= $comment->chapterId ?>" />
@@ -43,26 +45,26 @@
 		<h2>Laisser un commentaire</h2>
 		<form class="alt" method="post" action="?action=addComment">
 			<div class="row uniform">
-			<div class="6u 12u$(xsmall)">
-				<label for="author">Nom</label>
-				<input id="name" name="author" type="text" />
+				<div class="6u 12u$(xsmall)">
+					<label for="author">Nom</label>
+					<input id="name" name="author" type="text" />
+				</div>
+				<div class="6u 12u$(xsmall)">
+					<label for="authorMail">Mail</label>
+					<input id="mail" name="authorMail" type="text" />
+				</div>
+				<div class="12u$">
+					<label for="content">Commentaire</label>
+					<textarea id="comment" name="content" rows="3" /></textarea>
+				</div>
+				<input type="hidden" name="chapterid" value="<?= $chapter->id() ?>" />
+				<div class="12u$">
+					<ul class ="actions">
+						<li>
+							<input class="special" value="Laisser un commentaire" type="submit" />
+						</li>
+					</ul>
+				</div>
 			</div>
-			<div class="6u 12u$(xsmall)">
-				<label for="authorMail">Mail</label>
-				<input id="mail" name="authorMail" type="text" />
-			</div>
-			<div class="12u$">
-				<label for="content">Commentaire</label>
-				<textarea id="comment" name="content" rows="3" /></textarea>
-			</div>
-			<input type="hidden" name="chapterid" value="<?= $chapter->id() ?>" />
-			<div class="12u$">
-			<ul class ="actions">
-				<li>
-					<input class="special" value="Laisser un commentaire" type="submit" />
-				</li>
-			</ul>
-		</div>
-		</div>
 		</form>	
 	</section>	
