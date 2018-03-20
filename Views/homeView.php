8653a83b26e045655393bc68503e2e9c1dfea167
Caroline Hugues <hugues.caroline9@gmail.com>
2018-03-20 12:39:33 +0100

    Amélioration de la présentation

--- Views/homeView.php
+++ Views/homeView.php
@@ -5,11 +5,13 @@
 	<header class="major">
 		<h2>Présentation</h2>
 	</header> 
-
-	<p>Un livre ... <br>
-		<a class="image main" href="/project3_openclassrooms/index.php?action=listChapters">
-			<img src="Public/images/cross-country-skiers-1591117_1280.jpg" alt="Photo de présentation" /> 
-		</a>
-	</p>
+	<section>
+		<p id="presentation"> Bonjour, <br> je suis Jean Forteroche, acteur et écrivain. J'ai décidé de me lancer dans une nouvelle aventure, <i>et la partager avec vous</i>, en vous présentant mon nouveau roman <i>Billet simple pour l'Alaska</i> entièrement sur ce site internet. Un nouveau chapitre est publié quotidiennement.<br>
+			<a id="link-presentation" class="image main" href="/project3_openclassrooms/index.php?action=listChapters">
+				<img src="Public/images/cross-country-skiers-1591117_1280.jpg" alt="Photo de présentation" /> 
+				Lire <i>Billet simple pour l'Alaska</i>
+			</a>
+		</p>
+	</section>
 
 
