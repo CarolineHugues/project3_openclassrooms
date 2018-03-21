93389c4cbda4187e74831b5bf17e8ebe16b22a51
Caroline Hugues <hugues.caroline9@gmail.com>
2018-03-21 21:24:15 +0100

    Séparation en classes frontendview et backendview

---
+++ Views/Backend/backendview.php
@@ -0,0 +1,18 @@
+<?php 
+require_once 'Views/view.php';
+
+class BackendView extends View {
+
+  public function __construct($action) 
+  {
+    $this->file = "Views/Backend/" . $action . "View.php";
+  }
+
+  public function generate($data) 
+  {
+    $content = $this->generateFile($this->file, $data);
+    $view = $this->generateFile('Views/Backend/template.php', array('title' => $this->title, 'content' => $content));
+    
+    echo $view;
+  }
+}
