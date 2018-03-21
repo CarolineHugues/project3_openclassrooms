2027ecfaa3af7f4fdc18441d52bc809f7808be10
Caroline Hugues <hugues.caroline9@gmail.com>
2018-03-21 11:45:13 +0100

    Amélioration des fonctions

--- Models/connection.php
+++ Models/connection.php
@@ -8,30 +8,6 @@ class Connection
 
 	const INVALID_PASSWORD = 1;
 
-	public function isValid() 
-	{
-		return !empty($_POST['login']) && !empty($_POST['password']) &&  $this->correspond() != 0 /*Le login correspond au mot de passe*/;
-	}
-
-	public function correspond()
-	{
-		if (password_verify($_POST['password'], password()))
-		{
-			$request = $this->db->prepare('SELECT COUNT(*) AS valid_correspondence FROM connection WHERE password = :password AND login = :login'); 
-
-			$request->bindValue(':password', password_hash($_POST['password']), PDO::PARAM_STR);
-			$request->bindValue(':login', $_POST['login'], PDO::PARAM_STR);
-
-			$data = $request->execute();
-
-			return = $data; 
-		}
-		else 
-		{
-			return = 0;
-		} 
-	}
-
 	// SETTERS
 	
 	public function setPassword($password) 
@@ -49,12 +25,12 @@ class Connection
 
 	public function errors()
 	{
-		return this->_errors;
+		return $this->_errors;
 	}
 
 	public function password()
 	{
-		return this->_password;
+		return $this->_password;
 	}
 
 }
--- Models/connectionmanager.php
+++ Models/connectionmanager.php
@@ -2,7 +2,6 @@
 
 class ConnectionManager extends Manager 
 {
-
 	public function update(Connection $connection) 
 	{
 		$request = $this->db->prepare('UPDATE connection SET password = :password WHERE login = :login');
@@ -12,5 +11,27 @@ class ConnectionManager extends Manager
 		$request->execute();
 
 		$request->closeCursor(); 
+	}
+
+	public function isValid($login, $password) 
+	{
+		return !empty($login) && !empty($password) && $this->correspond($login, $password) == true;
+	}
+
+	public function correspond($login, $password)
+	{   
+		$request = $this->db->prepare('SELECT * FROM connection WHERE login = :login');
+		$request->execute(array('login' => $login));
+		$data = $request->fetch();
+
+		if ($login != $data['login'])
+		{
+			return false;
+		}
+		else 
+		{
+			$isPasswordCorrect = password_verify($password, $data['password']);
+			return $isPasswordCorrect;
+		}
 	}
 }
