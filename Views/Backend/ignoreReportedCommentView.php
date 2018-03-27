cca8cb6e5c328416a00c7ccee49624f8b583a8da
Caroline Hugues <hugues.caroline9@gmail.com>
2018-03-27 12:16:53 +0200

    Ajout de la vue après avoir ignoré un commentaire signalé

---
+++ Views/Backend/ignoreReportedCommentView.php
@@ -0,0 +1,4 @@
+<?php $this->title = 'Le commmentaire n\'est plus signalé'; ?>
+
+<p>Le commmentaire n'est plus signalé ! </p>
+<a href="?action=adminAccess"><button>Retour à l'accueil de l'administration du site</button></a>
