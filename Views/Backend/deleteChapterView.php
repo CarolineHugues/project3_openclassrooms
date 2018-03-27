e6a97fefe0ce4f575c119ba11e726e193ed4e1e0
Caroline Hugues <hugues.caroline9@gmail.com>
2018-03-27 12:18:18 +0200

    Ajout de la vue après la suppression d'un chapitre

---
+++ Views/Backend/deleteChapterView.php
@@ -0,0 +1,4 @@
+<?php $this->title = 'Le chapitre a été supprimé'; ?>
+
+<p>Le chapitre a été supprimé ! </p>
+<a href="?action=adminAccess"><button>Retour à l'accueil de l'administration du site</button></a>
