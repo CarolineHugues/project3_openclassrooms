b8b330825070738226a4c64f78b284f89eea6f93
Caroline Hugues <hugues.caroline9@gmail.com>
2018-03-27 12:17:47 +0200

    Ajout de la vue après la suppression d'un commentaire

---
+++ Views/Backend/deleteCommentView.php
@@ -0,0 +1,4 @@
+<?php $this->title = 'Le commentaire a été supprimé'; ?>
+
+<p>Le commentaire a été supprimé ! </p>
+<a href="?action=adminAccess"><button>Retour à l'accueil de l'administration du site</button></a>
