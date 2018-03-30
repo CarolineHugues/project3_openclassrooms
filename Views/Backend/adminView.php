0a2dde993ff4f82a9f85850b0f7e8e0102b40a93
Caroline Hugues <hugues.caroline9@gmail.com>
2018-03-30 13:29:10 +0200

    Amélioration de la vue du Backend

--- Views/Backend/adminView.php
+++ Views/Backend/adminView.php
@@ -10,13 +10,13 @@
 			<?php if ($nbPublishedChapters < 2)
 			{
 			?>
-				<h4 class="subTitlesLists"> <?= $nbPublishedChapters ?> chapitre publié :</h4>
+				<h4> <?= $nbPublishedChapters ?> chapitre publié :</h4>
 			<?php
 			}
 			else
 			{
 			?>
-				<h4 class="subTitlesLists"> <?= $nbPublishedChapters ?> chapitres publiés :</h4>
+				<h4> <?= $nbPublishedChapters ?> chapitres publiés :</h4>
 			<?php
 			}
 			?>
@@ -28,6 +28,9 @@
 						<a href="index.php?action=chapter&id=<?= $chapter->id() ?>"><h5 class="chaptersTitles"><?= $chapter->title ?></h5></a>
 						<p class="datesChaptersLists">Publié le <?= $chapter->publishedDate_fr ?> | Dernière modification le <?= $chapter->updateDate_fr ?> </p>
 						<div class="buttonsChaptersLists">
+							<form action="?action=chapter&id=<?= $chapter->id() ?>" method="post">
+								<input type="submit" value="Afficher" name="show" class="buttonAdmin" />
+							</form>
 							<form action="?action=writeChapter" method="post">
 								<input type="hidden" name="id" value="<?= $chapter->id() ?>" />
 								<input type="submit" value="Modifier" name="modify" class="buttonAdmin" />
@@ -47,13 +50,13 @@
 			<?php if ($nbDraftsChapters < 2)
 			{
 			?>
-				<h4 class="subTitlesLists"> <?= $nbDraftsChapters ?> chapitre enregistré en brouillon :</h4>
+				<h4> <?= $nbDraftsChapters ?> chapitre enregistré en brouillon :</h4>
 			<?php
 			}
 			else
 			{
 			?>
-				<h4 class="subTitlesLists"> <?= $nbDraftsChapters ?> chapitres enregistrés en brouillon :</h4>
+				<h4> <?= $nbDraftsChapters ?> chapitres enregistrés en brouillon :</h4>
 			<?php
 			}
 			?>
@@ -65,6 +68,9 @@
 						<a href="index.php?action=chapter&id=<?= $chapter->id() ?>"><h5 class="chaptersTitles"><?= $chapter->title ?></h5></a>
 						<p class="datesChaptersLists">Ajouté le <?= $chapter->addDate_fr ?> | Dernière modification le <?= $chapter->updateDate_fr ?></p>
 						<div class="buttonsChaptersLists">
+							<form action="?action=chapter&id=<?= $chapter->id() ?>" method="post">
+								<input type="submit" value="Apperçu" name="preview" class="buttonAdmin" />
+							</form>
 							<form action="?action=writeChapter" method="post">
 								<input type="hidden" name="id" value="<?= $chapter->id() ?>" />
 								<input type="submit" value="Modifier" name="modify" class="buttonAdmin" />
@@ -94,13 +100,13 @@
 			<?php if ($nbReportedComments < 2)
 			{
 			?>
-				<h4 class="subTitlesLists"> <?= $nbReportedComments ?> commentaire signalé :</h4>
+				<h4> <?= $nbReportedComments ?> commentaire signalé :</h4>
 			<?php
 			}
 			else
 			{
 			?>
-				<h4 class="subTitlesLists"> <?= $nbReportedComments ?> commentaires signalés :</h4>
+				<h4> <?= $nbReportedComments ?> commentaires signalés :</h4>
 			<?php
 			}
 			?>
@@ -109,11 +115,13 @@
 			?>
 				<ul>
 					<li>
-						<h5>Par <?= $comment->author ?> | Adresse mail : <?= $comment->authorMail ?></h5>
+						<h5>Par <?= htmlspecialchars($comment->author) ?> | Adresse mail : <?= htmlspecialchars($comment->authorMail) ?></h5>
 						<p class="dateCommentsLists">Le <?= $comment->publishedDate_fr ?></p>
-						<p class="commentsContent"><?= $comment->content ?></p>
-						<a href="index.php?action=chapter&id=<?= $comment->chapterId ?>"><p class="commentsChapter">Voir le chapitre correspondant</p></a>
+						<p class="commentsContent"><?= htmlspecialchars(nl2br($comment->content)) ?></p>
 						<div class="buttonsCommentsLists">
+							<form action="?action=chapter&id=<?= $comment->chapterId ?>" method="post">
+								<input type="submit" value="Afficher" name="show" class="buttonAdmin" />
+							</form>
 							<form action="?action=deleteComment" method="post">
 								<input type="hidden" name="id" value="<?= $comment->id() ?>" />
 								<input type="submit" value="Supprimer" name="delete" class="buttonAdmin" />
@@ -133,13 +141,13 @@
 			<?php if ($nbPublishedComments < 2)
 			{
 			?>
-				<h4 class="subTitlesLists"> <?= $nbPublishedComments ?> commentaire publié :</h4>
+				<h4 id="titlePublishedCommentsList"> <?= $nbPublishedComments ?> commentaire publié :</h4>
 			<?php
 			}
 			else
 			{
 			?>
-				<h4 class="subTitlesLists"> <?= $nbPublishedComments ?> commentaires publiés :</h4>
+				<h4 id="titlePublishedCommentsList"> <?= $nbPublishedComments ?> commentaires publiés :</h4>
 			<?php
 			}
 			?>
@@ -148,14 +156,18 @@
 			?>
 				<ul>
 					<li>
-						<h5>Par <?= $comment->author ?> | Adresse mail : <?= $comment->authorMail ?></h5>
+						<h5>Par <?= htmlspecialchars($comment->author) ?> | Adresse mail : <?= htmlspecialchars($comment->authorMail) ?></h5>
 						<p class="dateCommentsLists">Le <?= $comment->publishedDate_fr ?></p>
-						<p class="commentsContent"><?= $comment->content ?></p>
-						<a href="index.php?action=chapter&id=<?= $comment->chapterId ?>"><p class="commentsChapter">Voir le chapitre correspondant</p></a>
+						<p class="commentsContent"><?= htmlspecialchars(nl2br($comment->content)) ?></p>
+						<div class="buttonsCommentsLists">
+							<form action="?action=chapter&id=<?= $comment->chapterId ?>" method="post">
+								<input type="submit" value="Afficher" name="show" class="buttonAdmin" />
+							</form>
 							<form action="?action=deleteComment" method="post">
 								<input type="hidden" name="id" value="<?= $comment->id() ?>" />
 								<input type="submit" value="Supprimer" name="delete" class="buttonAdmin" />
 							</form>
+						</div>
 					</li>
 				</ul>
 			<?php 		
@@ -166,4 +178,17 @@
 
 <section class="sectionAdmin">
 	<h3 class="titleAdmin">Login et mot de passe</h3>
+	<section id="updatePasswordSpace">
+		<div id="showLogin">
+			<h4>Login :</h4>
+			<p><?= $getLogin ?></p>	
+		</div>
+		<h4>Changer de mot de passe :</h4>
+		<form action="?action=updatePassword" method="post">
+			<input type="password" name="password" placeholder="Renseigner votre nouveau mot de passe" class="passwordField" /><br />
+			<input type="password" name="newpassword" placeholder="Renseigner à nouveau votre nouveau mot de passe" class="passwordField"/><br />
+			<input type="hidden" name="login" value="<?= $getLogin ?>" />
+			<input type="submit" value="Modifier" name="updatePassword" class="buttonAdmin" />
+		</form>
+	</section>
 </section>
