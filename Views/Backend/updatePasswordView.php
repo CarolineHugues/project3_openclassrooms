991be65b4686d269f3324c04ef067a1c8814f526
Caroline Hugues <hugues.caroline9@gmail.com>
2018-03-30 13:25:29 +0200

    Ajout de la vue pour confirmer la modification du mot de passe

---
+++ Views/Backend/updatePasswordView.php
@@ -0,0 +1,6 @@
+<?php $this->title = 'Le mot de passe a été modifié'; ?>
+
+<section class="confirmationPage">
+	<p class="confirmationPageMessage">Le mot de passe a été modifié ! </p>
+	<a href="?action=adminAccess"><button class="buttonAdmin2">Retour à l'accueil de l'administration du site</button></a>
+</section>
