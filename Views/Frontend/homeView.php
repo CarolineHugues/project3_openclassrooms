66e452d5fbb20b415d77b2af7c634753bae4e842
Caroline Hugues <hugues.caroline9@gmail.com>
2018-04-09 18:22:08 +0200

    Ajout du résumé du roman sur la page d'accueil

--- Views/Frontend/homeView.php
+++ Views/Frontend/homeView.php
@@ -6,7 +6,13 @@
 		<h2>Présentation</h2>
 	</header> 
 	<section>
-		<p id="presentation"> Bonjour, <br> je suis Jean Forteroche, acteur et écrivain. J'ai décidé de me lancer dans une nouvelle aventure, <i>et la partager avec vous</i>, en vous présentant mon nouveau roman <i>Billet simple pour l'Alaska</i> entièrement sur ce site internet. Un nouveau chapitre est publié quotidiennement.<br>
+		<p class="presentation"> Bonjour, <br> je suis Jean Forteroche, acteur et écrivain. J'ai décidé de me lancer dans une nouvelle aventure, <i>et la partager avec vous</i>, en vous présentant mon nouveau roman <i>Billet simple pour l'Alaska</i> entièrement sur ce site internet. Un nouveau chapitre est publié quotidiennement.</p>
+		<h3 id="excerptTitle">Résumé :</h3>
+		<p id="excerpt">
+		Dan, James et Phillip, trois londoniens en quête d’aventures. Poussés par le même rêve : partir vivre en Alaska ; ils se lient d’amitié et décident de réaliser ensemble ce long périple : se rendre à Juneau de Montréal, à pied. Tel un carnet de voyage, Dan prend la plume pour raconter ce projet ambitieux, de sa conception à l’arrivée à Juneau.</p>
+	</section> 
+	<section>
+		<p class="presentation">
 			<a id="link-presentation" class="image main" href="/project3_openclassrooms/index.php?action=listChapters&p=1">
 				<img src="Public/images/cross-country-skiers-1591117_1280.jpg" alt="Photo de présentation" /> 
 				Lire <i>Billet simple pour l'Alaska</i>
